cbe2ffda6a947550f91b4f76fa58a03c9bc2eb80
shreesh30 <shreesharya20@gmail.com>
2025-01-12 21:41:26 +0530

    Homepage changes

---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,13 @@
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+
+const HomePage = () => {
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  );
+};
+
+export default HomePage;
